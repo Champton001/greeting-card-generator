9e6fe43a91b6bcca9920da4e29bf440b711986a6
Chase <you@example.com>
2026-09-03 22:30:57 -0500

    challenge cards

--- scripts/madlibs.js
+++ scripts/madlibs.js
@@ -1,12 +1,22 @@
 let name = prompt("A name:");
 let adjective = prompt("An adjective:");
 let noun = prompt("A noun:");
+let place = prompt("A place:");
+let food = prompt("A food:");
 
 function makeCard() {
   console.log("Dear " + name + ",");
   console.log("You are as " + adjective + " as a " + noun + ".");
-  console.log("Happy birthday from the generator.");
+  console.log("I hope you have fun at " + place + ",");
+  console.log("Enjoy some " + food + "!");
+  console.log("Have an awesome day generator.");
+  
 }
 
+function makeThankYouCard() {
+	console.log("Dear " + name + ",");
+	console.log("Thank you for being a great friend! ");
+	console.log("Lets meet at " + place + " and eat " + food + "!");
+}
 makeCard();
-makeCard();
+makeThankYouCard();

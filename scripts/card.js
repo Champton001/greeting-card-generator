a72498c993adf6729ea1724e444c9f5ccb9b4c8d
Chase <you@example.com>
2026-09-16 13:17:47 -0500

    random closing on the page closes #4

--- scripts/card.js
+++ scripts/card.js
@@ -1,19 +1,29 @@
-alert("hello world");
+
 
 let nameInput = document.getElementById("name-input");
 let adjectiveInput = document.getElementById("adjective-input");
 let nounInput = document.getElementById("noun-input");
 let makeButton = document.getElementById("make-button");
 let cardText = document.getElementById("card-text");
+let cardClosings = ["Goodbye!", "See you later!", "Take care!", "Have a great day!", "Adios!"]
+
 
 function makeCard() {
-  let friendName = nameInput.value;
+  let name = nameInput.value;
   let adjective = adjectiveInput.value;
   let noun = nounInput.value;
+  let index = randomNumber(0, cardClosings.length - 1);
+  let randomClosing = cardClosings[index];
+  console.log(randomClosing);
   cardText.textContent =
-    "Dear " + personReading + ", I really enjoy " + adjective +
-    " by  " + noun + ". Thank you for listening. You Rock!!! ";
+    "Dear " + name + ", I really enjoy " + adjective +
+    " by  " + noun + ". " + randomClosing ;
 }
+
+function randomNumber(a,b){
+	return Math.floor(Math.random() * (b - a + 1))
+}
+
 
 makeButton.addEventListener("click", makeCard);
 
